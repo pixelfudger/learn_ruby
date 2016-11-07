c1148e32afb975024bc562aa1bc8d75427307e1a
Harrison Ly <harriko@gmail.com>
2016-11-07 22:34:52 +0000

    solved problems 00 to 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+def add (a, b)
+  a + b
+end
+
+def subtract (a, b)
+  a - b
+end
+
+def sum (arr)
+  total = 0
+  if arr.length != 0
+    arr.each do |i|
+      total = total + i
+    end
+  end
+  total
+end
+
+def multiply (*numbers)
+  total = 1
+  numbers.each do |i|
+    total *= i
+  end
+  total
+end
+
+def power (a, b)
+  a ** b
+end
+
+def factorial (num)
+  total = 1
+  while num > 1
+    total *= num
+    num -= 1
+  end
+  total
+end
