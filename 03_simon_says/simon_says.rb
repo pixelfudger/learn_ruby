89b037e40530d5f066c56c5fb11ee934a3887b27
Harrison Ly <harriko@gmail.com>
2016-11-09 07:23:32 +0000

    solutions to 03-04

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo (says)
+  says
+end
+
+def shout (says)
+  says.upcase
+end
+
+def repeat (says, num = 2)
+  [says] * num * ' '
+end
+
+def start_of_word (says, num)
+  says[0..num-1]
+end
+
+def first_word (says)
+  says.split(' ').first
+end
+
+def titleize (says)
+ words = says.split.map do |w|
+   lowerWords = %(the and over)
+   if lowerWords.include?(w)
+     w
+   else
+     w.capitalize
+   end
+ end
+ words.first.capitalize!
+ words.join(' ')
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate (words)
+  vowels = %w(a e i o u)
+  newWords = []
+  words = words.split(' ')
+  words.each do |word|
+    if word[0..1] == 'qu'
+      newWords.push(translate(word[2..-1] + 'qu'))
+    elsif !vowels.include?(word[0])
+      word += word[0]
+      word[0] = ''
+      newWords.push(translate(word))
+    else
+      newWords.push(word += 'ay')
+    end
+  end
+  newWords.join(' ')
+end
