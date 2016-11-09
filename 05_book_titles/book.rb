c8b553aa99ee6a3454c7e4dda660b94ad4dfee0a
Harrison Ly <harriko@gmail.com>
2016-11-09 23:42:54 +0000

    solution for 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+  def title=(text)
+    smallWords = %w(and in the of a an)
+    text = text.split(' ')
+    @bookTitle = [text[0].capitalize]
+    text[1..-1].map do |word|
+      if !smallWords.include?(word)
+        word = word.capitalize
+      end
+      @bookTitle << word
+    end
+  end
+
+  def title
+    @bookTitle.join(' ')
+  end
 end
