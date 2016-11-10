a7d961e9d11e781750c7550f50883100b18a0cd5
Harrison Ly <harriko@gmail.com>
2016-11-10 22:30:00 +0000

    solution 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    sec = @seconds % 60
+    hour = @seconds / 3600
+    minutes = (@seconds / 60) - (hour * 60)
+    sprintf('%02d:%02d:%02d', hour, minutes, sec)
+  end
 end
